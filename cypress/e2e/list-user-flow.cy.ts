519fc563c65b3c25f1bb3606a5967f26d7f386c7
Danijel Grabež <danijel.grabez@sesamecare.com>
2022-06-22 14:27:50 +0200

    Add note on Cypress + MSW

--- cypress/e2e/list-user-flow.cy.ts
+++ cypress/e2e/list-user-flow.cy.ts
@@ -3,4 +3,6 @@ describe('empty spec', () => {
     cy.visit('http://localhost:3000/');
     cy.get('ul > :nth-child(10)').should('exist');
   });
+
+  // NOTE: Cypress can be configured to intercept network request and mock response (on individual spec, or global level)
 });
